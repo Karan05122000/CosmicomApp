0f61ef11338c2547504523d0a3138aa6634a781c
karan kangude <karankangude17@gmail.com>
2020-11-29 14:11:57 +0530

    retrofit added

--- app/src/main/java/com/example/cosmicom/login.java
+++ app/src/main/java/com/example/cosmicom/login.java
@@ -5,17 +5,31 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
+import android.widget.Spinner;
 
-public class login extends AppCompatActivity {
+import java.util.HashMap;
+import java.util.Map;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class login extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     EditText mEmail,mPassword;
     Button mLoginBtn;
     ProgressBar progressBar;
-
+    String user_type_selection;
+    UserInterface userInterface;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,6 +37,19 @@ public class login extends AppCompatActivity {
         mEmail = findViewById(R.id.email_et);
         mPassword = findViewById(R.id.password_et);
         progressBar = findViewById(R.id.progressBar);
+        Spinner spinner = (Spinner) findViewById(R.id.user_type);
+// Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.user_type, android.R.layout.simple_spinner_item);
+// Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+// Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(getResources().getString(R.string.base_url))
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        userInterface = retrofit.create(UserInterface.class);
         mLoginBtn=findViewById(R.id.loginButton);
         mLoginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -37,14 +64,35 @@ public class login extends AppCompatActivity {
                     mPassword.setError("Password is Required.");
                     return;
                 }
-                if(password.length() < 6){
-                    mPassword.setError("Password Must be >= 6 Characters");
-                    return;
-                }
-                progressBar.setVisibility(View.VISIBLE);
 
+                progressBar.setVisibility(View.VISIBLE);
+                User user  =  new User(email,user_type_selection,password);
+                Map<String, String> fields = new HashMap<>();
+                fields.put("email", email);
+                fields.put("user_type", user_type_selection);
+                fields.put("password", password);
+                Call<User>call=userInterface.createPost(fields);
+                call.enqueue(new Callback<User>() {
+                    @Override
+                    public void onResponse(Call<User> call, Response<User> response) {
+                        Log.d("wtf", String.valueOf(response.code()));
+                    }
+                    @Override
+                    public void onFailure(Call<User> call, Throwable t) {
+
+                    }
+                });
             }
         });
     }
 
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        user_type_selection = parent.getItemAtPosition(position).toString();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
 }
